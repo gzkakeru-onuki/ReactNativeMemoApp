569b74b7659d928e269a965201a9cfd5fb0c9882
gzkakeru-onuki <gzdevonuki@gmail.com>
2025-02-22 16:56:06 +0900

    Typescriptの基本の学習

--- Dev/udemy/MemoApp/App.tsx
+++ Dev/udemy/MemoApp/App.tsx
@@ -1,7 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = ():JSX.Element => {
+  
+  
+
+  
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!!!</Text>
@@ -19,3 +24,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App
